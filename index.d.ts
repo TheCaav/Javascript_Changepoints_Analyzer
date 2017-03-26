e22685a674a1ddf4a17ddfb06264126f93fba0f7
Calvin Urankar <uuead@student.kit.edu>
2017-03-26 21:51:00 +0200

    Updated Typedefinitions. They dont seem to work tho

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,18 @@
-// Type defiinitions for JavascriptChangepointsAnalyzer
+// Type definitions for Javascript_Changepoints_Analyzer
+// Project: Javascript_Changepoints_Analyzer
+// Definitions by: Calvin Urankar
 
-export function analyze(arr: Array<{y: number, x: number}>): Array<number>;
-export function setSettings(maxCount: number, useSquared: boolean);
+declare function analyze(dt: analyze.Datapoint[], settings: analyze.Settings): number[];
+export = analyze;
+export as namespace changepointsAnalyzer;
+declare namespace analyze {
+  export interface Datapoint {
+    x: number;
+    y: number;
+  }
+
+  export interface Settings {
+    maxCount: number;
+    useSquared: boolean;
+  }
+}
